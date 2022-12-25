da534c161c73faca2ca08cd8bae85ac47da1f7a5
Hamid Safdari <hamidsafdari22@gmail.com>
2022-12-25 21:05:08 +0430

    base app

---
+++ src/main/kotlin/hs/spring/k8s/App.kt
@@ -0,0 +1,11 @@
+package hs.spring.k8s
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
